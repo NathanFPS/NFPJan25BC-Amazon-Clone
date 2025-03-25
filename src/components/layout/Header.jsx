ebbf5c85860635dc0456837adcb27955ac2b69c0
NathanFPS <nathanaelfp94@gmail.com>
2025-03-25 22:31:26 +0200

    3rd

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Header = () => {
-  return (
-    <header class='header'>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  );
+    return (
+      <>
+        <header className="header">
+          <Link to="/">
+            <img className="header-logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo" />
+          </Link>
+          <div className="search-section">
+            <input className="search-input" type="text" />
+            <SearchIcon className='search-icon' />
+          </div>
+          <div className="header-nav">
+            <div className="header-options">
+                <span className="option-1">Hello Guest</span>
+                <span className="option-2">Sign In</span>
+            </div>
+            
+            <div className="header-options">
+                <span className="option-1">Returns</span>
+                <span className="option-2">And Orders</span>
+            </div>
+
+            <div className="header-options">
+                <span className="option-1">Your</span>
+                <span className="option-2">Prime</span>
+            </div>
+
+            <div className="header-basket">
+                <ShoppingCartOutlinedIcon />
+                <span className="basket-count">0</span>
+            </div>
+          </div>
+        </header>
+
+
+      </>
+    );
 }
 
 export default Header;
