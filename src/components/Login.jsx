abd2c0280194078d8108c8dc7986fb6738e2fbe7
NathanFPS <nathanaelfp94@gmail.com>
2025-03-26 18:28:06 +0200

    6th

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,50 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useReducer, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import { Link, Route } from 'react-router-dom';
 import Modal from './Modal';
 import './styles/Login.css';
 
+const reducer = (state, action) => {
+  if(action.type === 'EMAIL_INPUT') {
+    return {...state, emailValue: action.payload};
+  }
+  if(action.type === 'PASSWORD_INPUT') {
+    return {...state, passwordValue: state.passwordValue};
+  }
+  return {emailValue: "", passwordValue: ""};
+}
+
 const Login = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    emailValue: "", 
+    passwordValue: ""
+  });
+  const {emailValue: email, passwordValue: password} = state;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(email.includes('@') && state.password.trim().length > 6);
+    }, 500);
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    }
+  }, [email, password]);
+
+  const emailChangeHandler = (e) => {
+    dispatch({type: 'EMAIL_INPUT', payload: e.target.value});
+  }
+
+  const passwordChangeHandler = (e) => {
+    dispatch({type: 'PASSWORD_INPUT', payload: e.target.value});
+  }
 
   const signIn = (e) => {
     e.preventDefault();
-    const enteredEmail = emailRef.current.value;
-    const enteredPassword = passwordRef.current.value;
-    console.log("Email: ", enteredEmail + "Password: ", enteredPassword);
+    console.log("Entered Email: ", state.emailValue);
+    console.log("Entered Password: ", state.passwordValue);
   }
 
   return (
@@ -25,9 +57,15 @@ const Login = () => {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" ref={emailRef} />
+          <input type="text" 
+            value={state.emailValue}
+            onChange={emailChangeHandler}
+          />
           <h5>Password</h5>
-          <input type="password" ref={passwordRef}/>
+          <input type="password" 
+            value={state.passwordValue}
+            onChange={passwordChangeHandler}
+          />
           <button type="submit" className="login-signInButton">Sign In</button>
         </form>
         <p>
